13cdca38fa6d8177e3d43364ccaf94ab2aba7261
jcampbell57 <jcampbell57@gmail.com>
2023-11-07 15:33:42 -0800

    show recent flickr uploads when no flickr_id given

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new
-    return unless params[:flickr_id]
 
-    @photos = flickr.people.getPublicPhotos(user_id: params[:flickr_id], api_key: ENV['FLICKR_API_KEY'])
+    @photos = if params[:flickr_id]
+                flickr.people.getPublicPhotos(user_id: params[:flickr_id], api_key: ENV['FLICKR_API_KEY'])
+              else
+                @photos = flickr.photos.getRecent(api_key: ENV['FLICKR_API_KEY'], per_page: 48)
+              end
   end
 end
