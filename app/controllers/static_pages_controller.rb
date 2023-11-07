5c8fb1f515e19575b9bd53a1d653a4196fa3e2ee
jcampbell57 <jcampbell57@gmail.com>
2023-11-07 15:24:25 -0800

    add StaticPages controller and view with working Flickr Finder

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
+    flickr = Flickr.new
+    return unless params[:flickr_id]
+
+    @photos = flickr.people.getPublicPhotos(user_id: params[:flickr_id], api_key: ENV['FLICKR_API_KEY'])
   end
 end
