d759fbecaddf7ecfd31703c6dcac91a40aaa82d8
jcampbell57 <jcampbell57@gmail.com>
2023-11-08 11:00:45 -0800

    add flash alerts, fix bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,16 @@ class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new
 
-    @photos = if params[:flickr_id]
-                flickr.people.getPublicPhotos(user_id: params[:flickr_id], api_key: ENV['FLICKR_API_KEY'])
-              else
-                @photos = flickr.photos.getRecent(api_key: ENV['FLICKR_API_KEY'], per_page: 48)
-              end
+    if params[:flickr_id]
+      begin
+        @photos = flickr.people.getPublicPhotos(user_id: params[:flickr_id],
+                                                api_key: ENV['FLICKR_API_KEY'])
+      rescue StandardError => e
+        flash.now[:alert] = "#{e.class}: #{e.message}. Please try again..."
+        @photos = flickr.photos.getRecent(api_key: ENV['FLICKR_API_KEY'], per_page: 48)
+      end
+    else
+      @photos = flickr.photos.getRecent(api_key: ENV['FLICKR_API_KEY'], per_page: 48)
+    end
   end
 end
